5aed9879ab5ac84dfa4151d46333b6b5254aaabc
freddyjpb <114787879+freddyjpb@users.noreply.github.com>
2023-01-07 04:45:32 -0600

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,64 @@
-import './App.css'
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import ErrorLocation from './components/ErrorLocation';
+import LocationInfo from './components/LocationInfo';
+import ResidentInfo from './components/ResidentInfo';
+
+import './App.css';
 
 function App() {
-  
+  const [location, setLocation] = useState();
+  const [locationInput, setLocationInput] = useState();
+  const [errorLocation, setErrorLocation] = useState( false );
+
+  useEffect(() => {
+    let locationURL;
+
+    if (locationInput) {
+      locationURL = `https://rickandmortyapi.com/api/location/${locationInput}`;
+    } else {
+      const randomLocation = Math.floor(Math.random() * 126) + 1;
+      locationURL = `https://rickandmortyapi.com/api/location/${randomLocation}`;
+    }
+
+    axios.get(locationURL)
+      .then(res => {
+        setErrorLocation( false );
+        setLocation( res.data );
+      })
+      .catch(err => {
+        setErrorLocation( true );
+        console.log( err );
+      });
+  }, [locationInput]);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    //console.log( e.target.inputSearch.value );
+    setLocationInput(e.target.inputSearch.value);
+  };
+
   return (
     <div className="App">
-      <div className="App__banner">
-      </div>      
+      <h1>Rick and Morty</h1>
+      <form onSubmit={handleSubmit}>
+        <input id='inputSearch' type="text" />
+        <button>Search</button>
+      </form>
+      {errorLocation ? (
+        <ErrorLocation />
+      ) : (
+        <div>
+          <LocationInfo location={location} />
+          <div className='app__residentcard--container'>
+            {
+              location?.residents.map(url => (
+                <ResidentInfo key={url} url={url} />
+              ))
+            }
+          </div>
+        </div>
+      )}
     </div>
   )
 }
---
+++ src/components/ErrorLocation.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ErrorLocation = () => {
+  return (
+    <div>
+        <h2>
+            You must have to enter a number betwen 0 and 126
+        </h2>
+    </div>
+  )
+}
+
+export default ErrorLocation
