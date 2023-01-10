5be27ec8d7677b0d88652096a9ba54240db9a44e
freddyjpb <114787879+freddyjpb@users.noreply.github.com>
2023-01-10 17:45:04 -0600

    Add files via upload

--- src/components/ErrorLocation.jsx
+++ src/components/ErrorLocation.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 
+import './ErrorLocation.css';
+
+import errorlogo from '../assets/errlogo.png';
+
 const ErrorLocation = () => {
   return (
     <div>
+      <div className='errorlocation'>
+        <img src={errorlogo} />
         <h2>
-            You must have to enter a number betwen 0 and 126
+          You must have to enter a number betwen 0 and 126
         </h2>
+      </div>
     </div>
   )
 }
